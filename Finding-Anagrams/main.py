15514725ee149e8c7bca989a8f28aa7592e73d09
Titilope Rotimi <titilopechisom20@gmail.com>
2022-06-09 20:06:08 -0700

    Add files via upload

---
+++ Finding-Anagrams/main.py
@@ -0,0 +1,13 @@
+# Check if two words are anagrams 
+# Example:
+# find_anagrams("hello", "check") --> False
+# find_anagrams("below", "elbow") --> True
+
+
+def find_anagram(word, anagram):
+    # [assignment] Add your code here
+    print(sorted(word) == sorted(anagram))
+    # return True
+
+
+find_anagram("tea", "eat")
